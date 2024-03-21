18ee0ac91130812fbb60a8a9aa84f32e9d14d4c6
blueheeya83 <blueheeya12@gmail.com>
2024-03-21 12:41:48 +0900

    2024.03.21

---
+++ 01express/src/routes/blogRouter.js
@@ -0,0 +1,105 @@
+const {Router} = require("express");
+const { default: mongoose } = require("mongoose");
+const {Blog} = require("../model/Blog");
+const { User } = require("../model/User");
+const blogRouter = Router();
+
+blogRouter.post("/",async function(req,res){
+    try{
+      const {title,content,islive,userID} = req.body;
+
+      let user = await User.findById(userID);
+      if(!user){
+        return res.status(400).send({error:"유저가 없어요!!!"})
+      }
+
+      let blog = new Blog({...req.body,user}); //... 딥카피
+      await blog.save();
+      return res.send({blog});
+    
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+      }
+
+})
+blogRouter.get("/",async function(req,res){
+    try{
+      const blogs = await Blog.find({});
+      return res.send({ blogs })
+    
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+      }
+
+})
+blogRouter.get("/:blogId",async function(req,res){
+    try{
+      const {blogId} = req.params;
+      const blog = await Blog.findOne({_id:blogId})
+      return res.send({ blog })
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+      }
+
+})
+blogRouter.delete("/:blogId",async function(req,res){
+    try{
+      let {blogId} = req.params;
+
+      if(!mongoose.isValidObjectId(blogId)){
+        return res.status(400).send({error:"등록된 게시글이 없네요"})
+      }
+
+      const blog = await Blog.findByIdAndDelete({_id:blogId});
+      return res.send({blog});
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+      }
+
+})
+blogRouter.put("/:blogId",async function(req,res){
+    try{
+      let {blogId} = req.params;
+      const {title,content} = req.body
+
+      const blog =  await Blog.findByIdAndUpdate(
+        blogId,
+        {title,content},
+        {new:true});
+      return res.send({blog});
+
+      // let {blogId} = req.params;
+      // if(!mongoose.isValidObjectId(blogId)){
+      //   return res.status(400).send({error : "등록된 게시글이 없네요!!"})
+      // }
+
+      // let {title} = req.body;
+      // if (!title){
+      //   return res.status(400).send({error : "제목이 없네요! 제목을 입력해주세요!"})
+      // }
+      // const blog = await Blog.findByIdAndUpdate(
+      //   blogId,
+      //   {$set : {title}},
+      //   {new:true}
+      // )
+    
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+      }
+})
+blogRouter.patch("/:blogId/live",async function(req,res){
+  try{
+    const {blogId} = req.params;
+    const {islive} = req.body;
+
+    const blog = await Blog.findByIdAndUpdate(blogId,{islive},{new:true})
+    return res.send({blog})
+
+  
+  } catch (error) {
+      return res.status(500).send({ error: error.message })
+    }
+})
+
+
+module.exports = {blogRouter};
--- 01express/src/server.js
+++ 01express/src/server.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const { userRouter } = require('./routes/userRouter.js');
+const { blogRouter } = require('./routes/blogRouter.js');
 
 dotenv.config();
 //process.env.MONGO_URL
@@ -17,10 +18,11 @@ dotenv.config();
 async function server() {
   try {
     await mongoose.connect(process.env.MONGO_URL); //promise
-    console.log("db connected")
+    console.log("db connected");
+    mongoose.set("debug",true); //2024.03.21 추가 사항
     app.use(express.json()) //json 언어로 변경하기
-
     app.use("/user",userRouter)
+    app.use("/blog",blogRouter)
     
 
     app.listen(3000)
